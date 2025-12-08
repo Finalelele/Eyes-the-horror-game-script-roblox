04a74330fd9b18d60b2650a30660f718a5a76367
Finalelele <Akkountforgames@yandex.ru>
2025-12-08 16:30:25 +0500

    Create eyes script.lua

---
+++ eyes script.lua
@@ -0,0 +1,49 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "Eyes the horror game",
+   LoadingTitle = "Eyes",
+   LoadingSubtitle = "by Finalelele",
+   ConfigurationSaving = {
+      Enabled = false,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+
+local Tab = Window:CreateTab("Esp", 4483362458) -- Title, Image
+local Section = Tab:CreateSection("Esp")
+local Toggle = Tab:CreateToggle({
+   Name = "Esp krasue",
+   CurrentValue = false,
+   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+   if Value then
+		local krasue = workspace.Krasue
+		local espM = Instance.new("BoxHandleAdornment", krasue)
+		espM.Adornee = krasue
+		espM.AlwaysOnTop = true
+		espM.ZIndex = 0
+		espM.Size = Vector3.new(4, 4, 4)
+		espM.Transparency = 0.5
+		espM.Color3 = Color3.fromRGB(255, 0, 0)
+	else
+		--
+	end
+end
+})
