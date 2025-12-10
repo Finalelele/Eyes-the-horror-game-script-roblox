51cda93d5a4312b379d828311feb931dd62a55d2
Finalelele <Akkountforgames@yandex.ru>
2025-12-10 18:45:35 +0500

    Update eyes script.lua

--- eyes script.lua
+++ eyes script.lua
@@ -3,29 +3,30 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "Eyes the horror game",
-   LoadingTitle = "Eyes",
-   LoadingSubtitle = "by Finalelele",
-   ConfigurationSaving = {
-      Enabled = false,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
-   },
-   Discord = {
-      Enabled = false,
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-   KeySystem = false, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Untitled",
-      Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided",
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
+    Name = "Eyes the horror game",
+	LoadingTitle = "Eyes",
+    LoadingSubtitle = "by Finalelele",
+	
+    ConfigurationSaving = {
+       Enabled = false,
+       FolderName = nil, -- Create a custom folder for your hub/game
+       FileName = "Big Hub"
+    },
+    Discord = {
+       Enabled = false,
+       Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+    },
+    KeySystem = false, -- Set this to true to use our key system
+    KeySettings = {
+       Title = "Untitled",
+       Subtitle = "Key System",
+       Note = "No method of obtaining the key is provided",
+       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+    }
 })
 
 local Tab = Window:CreateTab("Esp", 4483362458) -- Title, Image
@@ -179,12 +180,35 @@ end,
 
 local Tab = Window:CreateTab("Other", 4483362458) -- Title, Image
 local Section = Tab:CreateSection("Doors")
-local Button = Tab:CreateButton({
-   Name = "Destroy Doors",
-   Callback = function()
-   	local Doors = workspace.Map.Doors
-	Doors:Destroy()
-end
+local Toggle = Tab:CreateToggle({
+   Name = "Hide Doors",
+   CurrentValue = false,
+   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+   if Value then
+		local defDoors = workspace.Map.Doors
+		for _,v in pairs(defDoors:GetDescendants()) do
+    		if  v.Name == "Door" then
+        		v.CanCollide = false
+				v.Transparency = 1
+   			 end
+		end
+		local exitDoor = workspace.Map.Doors.ExitDoor
+		exitDoor.CanCollide = false
+		exitDoor.Transparency = 1
+	else
+		local defDoors = workspace.Map.Doors
+		for _,v in pairs(defDoors:GetDescendants()) do
+    		if  v.Name == "Door" then
+        		v.CanCollide = true
+				v.Transparency = 0
+   			end
+		end
+		local exitDoor = workspace.Map.Doors.ExitDoor
+		exitDoor.CanCollide = true
+		exitDoor.Transparency = 0
+	end
+end,
 })
 
 local Section = Tab:CreateSection("Brightness")
@@ -203,12 +227,29 @@ local Toggle = Tab:CreateToggle({
 end,
 })
 
-local Button = Tab:CreateButton({
-   Name = "Destroy Flashlight",
-   Callback = function()
-   	local fl = workspace.Flashlight
-	fl:Destroy()
-	local rsa = game:GetService("ReplicatedStorage").Assets.Flashlight
-	rsa:Destroy()
-end
+local Toggle = Tab:CreateToggle({
+   Name = "Hide Flashlight",
+   CurrentValue = false,
+   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+   if Value then
+		local flashHand = workspace.Flashlight.FlashlightHandle
+		flashHand.Transparency = 1
+		local flashLight = workspace.Flashlight.FlashlightHandle.Light
+		for _,v in pairs(flashLight:GetDescendants()) do
+    		if  v.Name == "SpotLight" then
+        		v.Enabled = false
+   			end
+		end
+	else
+		local flashHand = workspace.Flashlight.FlashlightHandle
+		flashHand.Transparency = 0
+		local flashLight = workspace.Flashlight.FlashlightHandle.Light
+		for _,v in pairs(flashLight:GetDescendants()) do
+    		if  v.Name == "SpotLight" then
+        		v.Enabled = true
+   			end
+		end
+	end
+end,
 })
